5ba34cf183a6b7d54d7c19ee81e52cd58d992c72
MichelleDiaz06 <87913882+MichelleDiaz06@users.noreply.github.com>
2026-02-10 17:18:58 -0500

    xd

---
+++ Main/src/Bibliotecario.java
@@ -0,0 +1,14 @@
+public abstract class Bibliotecario extends Usuario {
+    private String turno;
+    public  Bibliotecario ( String id, String nombre, String email, String turno) {
+        super(id, nombre, email);
+        this.turno = turno;
+    }
+
+    public String darTurno(){
+        return this.turno;
+    }
+
+
+
+}
