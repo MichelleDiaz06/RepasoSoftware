c7195df88f34e61c767cc3458ff209fef3da0c71
MichelleDiaz06 <87913882+MichelleDiaz06@users.noreply.github.com>
2026-02-12 16:59:09 -0500

    xd

---
+++ Main/src/Biblioteca.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Socio> socios = new ArrayList<>();
+    private ArrayList<Bibliotecario> bibliotecarios = new ArrayList<>();
+
+    public Biblioteca () {}
+
+    public ArrayList<Socio> darSocios() {
+        return socios;
+    }
+
+    public ArrayList<Bibliotecario> darBibliotecarios() {
+        return bibliotecarios;
+    }
+
+    public void agregarSocio(Socio socio) {
+        socios.add(socio);
+    }
+
+    public void agregarBibliotecario(Bibliotecario bibliotecario) {
+        bibliotecarios.add(bibliotecario);
+    }
+
+
+}
